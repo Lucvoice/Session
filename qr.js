952697ff08426a4006ddc4b8f8b21f793155e489
Lucvoice <simkondabrand@gmail.com>
2026-04-17 23:49:11 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -126,7 +126,7 @@ const { makeid } = require('./id');
                           try {
                               await sock.sendMessage(sock.user.id, {
                                   text: `◈━━━━━━━━━━━◈
-  │❒ Hello! 👋 You're now connected to Toxic-MD.
+  │❒ Hello! 👋 You're now connected to LUCVOICE-XMD.
   │❒ Please wait a moment while we generate your session ID. It will be sent shortly... 🙂
   ◈━━━━━━━━━━━◈`
                               });
@@ -157,7 +157,7 @@ const { makeid } = require('./id');
                                       text: `◈━━━━━━━━━━━◈
   SESSION CONNECTED
 
-  │❒ The long code above is your Session ID. Please copy and store it safely, as you'll need it to deploy your Toxic-MD bot! 🔐
+  │❒ The long code above is your Session ID. Please copy and store it safely, as you'll need it to deploy your LUCVOICE-XMD bot! 🔐
 
   │❒ Need help? Reach out to us:
 
